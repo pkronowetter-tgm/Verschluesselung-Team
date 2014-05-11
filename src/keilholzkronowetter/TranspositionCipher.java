be88d9801a04781f4a31eea303bd5fdfc0328f35
Patricia Kronowetter <pkronowetter@student.tgm.ac.at>
2014-05-11 18:37:22 +0200

    Methode encrypt erstellt

--- src/keilholzkronowetter/TranspositionCipher.java
+++ src/keilholzkronowetter/TranspositionCipher.java
@@ -1,17 +1,55 @@
 package keilholzkronowetter;
 
 public class TranspositionCipher implements Cipher {
+	
+	private int level;
+	
+	public TranspositionCipher(int transpositionLevel){
+		this.level = transpositionLevel;
+	}
+	
+	public void setTranspositionLevel(int level){
+		this.level = level;
+	}
 
 	@Override
 	public String encrypt(String text) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		// das neue
+		char [] klartext = text.toCharArray();
+		int length = (int) StrictMath.ceil((double)text.length()/level);
+		
+		char [][] vArray = new char [level][length];
+		
+		int textIndex = 0;
+
+		for (int i = 0; i < length; i++) {
+			for (int j = 0; j < level; j++) {
+					if (textIndex < klartext.length) {
+						vArray[j][i] = klartext[textIndex];
+						textIndex++;
+					}
+			}
+		}
+		
+		String enc = "";
+		for(int i = 0; i<vArray.length; i++){
+			enc += new String(vArray[i]);
+		}
+
+		return enc;
 	}
 
 	@Override
 	public String decrypt(String text) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
-
+	public static void main(String[] args){// throws CipherException {
+		Cipher c = new TranspositionCipher(3);
+		String s = c.encrypt("Hallo das ist ein Test");
+		System.out.println(s);
+		s = c.decrypt(s);
+		System.out.println(s);
+	}
 }
