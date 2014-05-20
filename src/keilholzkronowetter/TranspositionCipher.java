9665fda89f0b1e5a6e8e4e6f9b96b8db403b0d22
Patricia Kronowetter <pkronowetter@student.tgm.ac.at>
2014-05-20 14:07:55 +0200

    Kommentiert

--- src/keilholzkronowetter/TranspositionCipher.java
+++ src/keilholzkronowetter/TranspositionCipher.java
@@ -1,8 +1,17 @@
 package keilholzkronowetter;
 
+/**
+ * 
+ * @author Patricia Kronowetter
+ * @version 2014-05-06
+ *
+ */
+
 public class TranspositionCipher implements Cipher {
 	
 	private int level;
+	
+	
 	
 	public TranspositionCipher(int transpositionLevel){
 		this.level = transpositionLevel;
@@ -11,17 +20,28 @@ public class TranspositionCipher implements Cipher {
 	public void setTranspositionLevel(int level){
 		this.level = level;
 	}
+	
+	/**
+	 * Die Methode encrypt dient dazu den eingebenen Text zu Verschlüsselt. Hierbei kann das Wort sowie das Level verändert werden.
+	 * 
+	 * @param text   Man übergibt den eingebenen text
+	 * 
+	 */
 
 	@Override
 	public String encrypt(String text) {
 		
-		// das neue
+		// Das Wort wird gesplittet und in ein Array abgespeichert
 		char [] klartext = text.toCharArray();
+		
+		// Die Länge des Wortes wird durch das Level herausgefunden ---> wieviele Buchstaben in due Arrays müssen.
 		int length = (int) StrictMath.ceil((double)text.length()/level);
 		
 		char [][] vArray = new char [level][length];
 		
+		// Index für das Wort, wird bei jeden Durchgang um 1 erhöht, (hilft damit man weiss an welchen Index man sich gerade befindet)
 		int textIndex = 0;
+		
 
 		for (int i = 0; i < length; i++) {
 			for (int j = 0; j < level; j++) {
@@ -31,7 +51,9 @@ public class TranspositionCipher implements Cipher {
 					}
 			}
 		}
-		
+		/**
+		 * Da wird das was jetzt in vArray ist in enc, reingespeichert.
+		 */
 		String enc = "";
 		for(int i = 0; i<vArray.length; i++){
 			enc += new String(vArray[i]);
@@ -39,15 +61,28 @@ public class TranspositionCipher implements Cipher {
 
 		return enc;
 	}
+	/**
+	 * Die Methode decrypt dient dazu den vorhin eingegeben Text zu entschlüsseln.
+	 * Dies habe ich genau so wie die Methode encrypt Programmiert, mit ein Paar kleinen Veränderungen.
+	 * 
+	 * @param text   Man übergibt den eingebenen text
+	 * 
+	 */
 
 	@Override
 	public String decrypt(String text) {
+		
+		// Das verschlüsselteWort wird gesplittet und in ein Array abgespeichert
 		char [] vText= text.toCharArray();
+		
+		// Die Länge des Wortes wird durch das Level herausgefunden ---> wieviele Buchstaben in due Arrays müssen.
 		int length = (int) StrictMath.ceil((double)text.length()/level);
 		
 		char [][] eArray = new char [level][length];
 		
+		// Index für das Wort, wird bei jeden Durchgang um 1 erhöht, (hilft damit man weiss an welchen Index man sich gerade befindet)
 		int textIndex = 0;
+		
 
 		for (int i = 0; i <eArray.length; i++) {
 			for (int j = 0; j <eArray[i].length; j++) {
@@ -55,24 +90,12 @@ public class TranspositionCipher implements Cipher {
 					textIndex++;
 			}
 		}
-		
 		String enc = "";
 		for(int i = 0; i<length; i++){
 			for(int j = 0; j<level; j++){
 				enc +=eArray[j][i];
 			}
 		}
-
 		return enc;
-		
 	}
-	/*
-	public static void main(String[] args){// throws CipherException {
-		Cipher c = new TranspositionCipher(3);
-		String s = c.encrypt("Hallo das ist ein Test");
-		System.out.println(s);
-		s = c.decrypt(s);
-		System.out.println(s);
-	}
-	*/
 }
